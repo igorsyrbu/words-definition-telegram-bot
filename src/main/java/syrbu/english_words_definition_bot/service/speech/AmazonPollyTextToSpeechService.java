83b47b76bd7a10dac47a3ef9b203bb11f538fa4c
Igor Syrbu <igorsyrbu90@gmail.com>
2021-02-21 16:21:56 +0200

    refactor amazon polly service

--- src/main/java/syrbu/english_words_definition_bot/service/speech/AmazonPollyTextToSpeechService.java
+++ src/main/java/syrbu/english_words_definition_bot/service/speech/AmazonPollyTextToSpeechService.java
@@ -1,22 +1,31 @@
-package syrbu.english_words_definition_bot.aws.service;
+package syrbu.english_words_definition_bot.service.speech;
 
 import com.amazonaws.services.polly.AmazonPolly;
 import com.amazonaws.services.polly.model.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.InputStream;
 import java.util.Optional;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
-public class AmazonPollyService {
+@ConditionalOnProperty(prefix = "telegram.bot", name = "speech-to-text", havingValue = "aws")
+public class AmazonPollyTextToSpeechService implements TextToSpeechService {
 
     private final AmazonPolly polly;
     private final Voice voice;
 
+    @PostConstruct
+    public void init(){
+        log.info("Amazon Polly text to speech service has been initialized");
+    }
+
+    @Override
     public Optional<InputStream> synthesize(String text) {
         try {
             SynthesizeSpeechRequest speechRequest = new SynthesizeSpeechRequest()
@@ -31,5 +40,4 @@ public class AmazonPollyService {
             return Optional.empty();
         }
     }
-
 }
